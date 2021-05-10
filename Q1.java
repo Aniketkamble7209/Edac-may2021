a23732f414a21c6d3015f590306355aa8fb33c2a
Aniket sampat kamble <83861882+Aniketkamble7209@users.noreply.github.com>
2021-05-10 22:11:21 +0530

    Add files via upload

--- Q1.java
+++ Q1.java
@@ -2,12 +2,7 @@ class Q1
 {
     public static void main(String[] args)
     {
-        for(int i=1;i<=6;i++)
-        {
-            for(int j=1;j<i;j++)
-            {
-                System.out.print(1+(j-1)+ " ");
-            }System.out.println();
-        }
+        System.out.println("Hello");
+        System.out.println("Aniket kamble");    
     }
 }
