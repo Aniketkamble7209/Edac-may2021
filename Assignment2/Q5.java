75b93fcf8f78160a805ffba7791b743bf9a146dc
Aniket sampat kamble <83861882+Aniketkamble7209@users.noreply.github.com>
2021-05-10 22:48:56 +0530

    Add files via upload

---
+++ Assignment2/Q5.java
@@ -0,0 +1,13 @@
+class Q5
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print((char)('A'+i)+" ");
+            }System.out.println();
+        }
+    }
+}
